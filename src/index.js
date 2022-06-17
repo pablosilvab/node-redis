2aced32b5fb3709e66a55c22726c7052ab4230f5
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-06-17 17:15:20 -0400

    consuming api with axios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const axios = require('axios')
+const responseTime = require('response-time')
+
 const app = express()
+app.use(responseTime())
+
+app.get('/civilizations', async (req, res) => {
+    const response = await axios.get('https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations')
+    res.json(response.data)
+})
+
 
 app.listen(3000)
 console.log('Server running on port 3000')
